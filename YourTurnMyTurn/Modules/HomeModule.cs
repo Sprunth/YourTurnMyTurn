257234ee62f10b265c958ed2848a404cf5003457
Dylan <sprunth@gmail.com>
2018-01-14 01:20:23 -0800

    basic setup

---
+++ YourTurnMyTurn/Modules/HomeModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace YourTurnMyTurn.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", o => "Hello World");
+            Get("/test/{name}", o => new TestPerson() {Name = o.name});
+        }
+    }
+
+    public class TestPerson
+    {
+        public string Name { get; set; }
+    }
+}
--- YourTurnMyTurn/Startup.cs
+++ YourTurnMyTurn/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Nancy.Owin;
 
 namespace YourTurnMyTurn
 {
@@ -25,10 +26,12 @@ namespace YourTurnMyTurn
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseOwin(x => x.UseNancy());
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
