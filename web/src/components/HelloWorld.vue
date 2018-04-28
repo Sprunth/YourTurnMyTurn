d6009dd06a1c2da74d3f14439a47f7c7229f259a
Dylan <sprunth@gmail.com>
2018-04-28 16:18:50 -0700

    minor cleanup

--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>d</h2>
     <input v-model="groupname">
     <button v-on:click="createGroup">Create Group</button>
     <br>
@@ -13,11 +12,18 @@
     <br>
     <h2>Group Members {{ groupname }}</h2>
     <button v-on:click="fetchGroupMembers">Fetch</button>
-    <ol>
-      <li v-for="gp in groupPersons" :key="gp.id">
-        {{ gp }}
-      </li>
-    </ol>
+    <br><br>
+    <table style="align: ">
+      <thead>
+        <th>Name</th><th>Contributed Value</th>
+      </thead>
+      <tbody>
+        <tr v-for="gp in groupPersons" :key="gp.id">
+          <td>{{ gp.name }}</td>
+          <td>{{ gp.contributedValue }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -37,7 +43,7 @@ export default {
   },
   methods: {
     createGroup() {
-      axios({ method: 'POST', url: 'http://localhost:56982/api/v1/group/' + this.groupname }).then((result) => {
+      axios({ method: 'POST', url: `http://localhost:56982/api/v1/group/${this.groupname}` }).then((result) => {
         this.groupid = result.data.slice(7); // ugh
         console.log('createGroup', result.data);
       }, (error) => {
@@ -45,23 +51,23 @@ export default {
       });
     },
     createPerson() {
-      axios({ method: 'POST', url: 'http://localhost:56982/api/v1/person/' + this.personname }).then((result) => {
+      axios({ method: 'POST', url: `http://localhost:56982/api/v1/person/${this.personname}` }).then((result) => {
         console.log('createPerson', result.data);
         this.addPersonToGroup(result.data.slice(8));
-        this.fetchGroupMembers();
+        this.fetchGroupMembers(); // todo: fix this race condition. then/finally/chain?
       }, (error) => {
         console.error(error);
       });
     },
     addPersonToGroup(personId) {
-      axios({ method: 'POST', url: 'http://localhost:56982/api/v1/group/' + this.groupid + '/add/' + personId }).then((result) => {
+      axios({ method: 'POST', url: `http://localhost:56982/api/v1/group/${this.groupid}/add/${personId}` }).then((result) => {
         console.log('addPersonToGroup', result.data);
       }, (error) => {
         console.error(error);
       });
     },
-    fetchGroupMembers(){
-      axios({ method: 'GET', url: 'http://localhost:56982/api/v1/group/' + this.groupid }).then((result) => {
+    fetchGroupMembers() {
+      axios({ method: 'GET', url: `http://localhost:56982/api/v1/group/${this.groupid}` }).then((result) => {
         this.groupPersons = result.data;
         console.log('fetchGroupMembers', result.data);
       }, (error) => {
@@ -88,4 +94,32 @@ li {
 a {
   color: #42b983;
 }
+
+table {
+  border: 2px solid #42b983;
+  border-radius: 3px;
+  background-color: #fff;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+th {
+  background-color: #42b983;
+  color: rgba(255,255,255,0.66);
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+td {
+  background-color: #f9f9f9;
+}
+
+th, td {
+  min-width: 120px;
+  padding: 10px 20px;
+}
+
 </style>
